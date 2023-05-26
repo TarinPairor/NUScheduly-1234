aa255dd7c297f7396f63da2a0fded60b05e05360
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2023-05-26 16:25:33 +0700

    Changed App.tsx: added homepage and loginpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,96 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { ChangeEvent, useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 function App() {
-  const [count, setCount] = useState(0);
-  // testing testing
-  //testing testing 2
-  // testing testing 3
+  const firebaseConfig = {
+    apiKey: "AIzaSyB6FPTWzjcvVa8QkjtSQgcMDXoXN_zl5z0",
+    authDomain: "fir-test1-d5129.firebaseapp.com",
+    projectId: "fir-test1-d5129",
+    storageBucket: "fir-test1-d5129.appspot.com",
+    messagingSenderId: "895677007234",
+    appId: "1:895677007234:web:efccf9a84d6e810db73b9b",
+    measurementId: "G-BHHCBREN6P",
+  };
+  initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleInputs = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputs = { [event.target.name]: event.target.value };
+    setData({ ...data, ...inputs });
+  };
+
+  const addData = () => {
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then(() => {
+        setIsLoggedIn(true);
+      })
+      .catch((error) => {
+        console.log("Login error:", error);
+      });
+  };
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        setIsLoggedIn(false);
+      })
+      .catch((error) => {
+        console.log("Logout error:", error);
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [auth]);
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App-header">
+      {isLoggedIn ? (
+        <>
+          <h1>Hello</h1>
+          <button onClick={handleLogout}>Log out</button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder="Email"
+            name="email"
+            type="email"
+            className="input-fields"
+            onChange={(event) => handleInputs(event)}
+          />
+          <input
+            placeholder="Password"
+            name="password"
+            type="password"
+            className="input-fields"
+            onChange={(event) => handleInputs(event)}
+          />
+          <button onClick={addData}>Log In</button>
+        </>
+      )}
+    </div>
   );
 }
 
