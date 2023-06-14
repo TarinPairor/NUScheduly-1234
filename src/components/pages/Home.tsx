9375d7ab88e54b15bc479fe6a21ce220ef7e641b
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2023-06-14 07:45:48 +0700

    attempt to use firestore db for hompage tasks

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,175 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import useFirebaseConfig from "../Firebase/useFirebaseConfig";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+
+interface Task {
+  id: string;
+  title: string;
+  status: boolean;
+}
+
+function Home() {
+  const [toDo, setToDo] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+  const [updateData, setUpdateData] = useState<Task | null>(null);
+
+  const userId = "qU19bsEdmdUoPkW2MxlbjDFyhGp1";
+  const db = getFirestore();
+  const tasksRef = collection(db, `users/${userId}/tasks`);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(tasksRef, (snapshot) => {
+      const tasks: Task[] = [];
+      snapshot.forEach((doc) => {
+        const task = doc.data() as Task;
+        tasks.push(task);
+      });
+      setToDo(tasks);
+    });
+
+    return () => unsubscribe();
+  }, [tasksRef]);
+
+  const addTask = async () => {
+    if (newTask) {
+      const num = toDo.length + 1;
+      const newEntry: Task = {
+        id: num.toString(),
+        title: newTask,
+        status: false,
+      };
+      await addDoc(tasksRef, newEntry);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = async (id: string) => {
+    try {
+      await deleteDoc(doc(tasksRef, id));
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  };
+
+  const cancelUpdate = () => {
+    setUpdateData(null);
+  };
+
+  const changeTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (updateData) {
+      const newEntry: Task = {
+        id: updateData.id,
+        title: e.target.value,
+        status: updateData.status,
+      };
+      setUpdateData(newEntry);
+    }
+  };
+
+  const updateTask = async () => {
+    if (updateData) {
+      const { id, title, status } = updateData;
+      const updatedTask = { title, status };
+      await updateDoc(doc(tasksRef, updateData.id), updatedTask);
+      setUpdateData(null);
+    }
+  };
+
+  return (
+    <div className="container App">
+      <br></br>
+      <h2>ToDolist</h2>
+      <br></br>
+      {/* Update task */}
+      {updateData && updateData ? (
+        <div className="row">
+          <div className="col">
+            <input
+              value={updateData && updateData.title}
+              onChange={(e) => changeTask(e)}
+              className="form-control form-control-lg"
+            />
+          </div>
+          <div className="col-auto">
+            <button
+              onClick={updateTask}
+              className="btn btn-lg btn-success mr-20"
+            >
+              Update Task
+            </button>
+            <button onClick={cancelUpdate} className="btn btn-lg btn-warning">
+              Cancel
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="row">
+          <div className="col">
+            <input
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              className="form-control form-control-lg"
+            />
+          </div>
+          <div className="col-auto">
+            <button onClick={addTask} className="btn btn-lg btn-success">
+              Add Task
+            </button>
+          </div>
+        </div>
+      )}
+
+      {toDo && toDo.length ? "" : "No Tasks..."}
+      {toDo &&
+        toDo.map((task, i) => {
+          return (
+            <React.Fragment key={task.id}>
+              <div className="col taskBg">
+                <div className={task.status ? "done" : ""}>
+                  {/*<span className="taskNumber">{i + 1}</span>*/}
+                  <span className="taskText">{task.title}</span>
+                </div>
+                <div className="iconsWrap">
+                  {task.status ? null : (
+                    <span
+                      title="Edit"
+                      onClick={() =>
+                        setUpdateData({
+                          id: task.id,
+                          title: task.title,
+                          status: task.status,
+                        })
+                      }
+                    >
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                  )}
+
+                  <span title="Trash" onClick={() => deleteTask(task.id)}>
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                  </span>
+                </div>
+              </div>
+            </React.Fragment>
+          );
+        })}
+    </div>
+  );
+}
+
+export default Home;
+/*
+
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
@@ -34,7 +206,7 @@ function Home() {
       return task;
     });
     setToDo(newTaskList);
-  };*/
+  };
 
   const cancelUpdate = () => {
     setUpdateData(null);
@@ -67,7 +239,7 @@ function Home() {
       <br></br>
       <h2>ToDolist</h2>
       <br></br>
-      {/*Update task*/}
+      {/*Update task}
       {updateData && updateData ? (
         <div className="row">
           <div className="col">
@@ -144,4 +316,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Home;*/
