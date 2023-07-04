b216452962450aea84fb573c82eb5dac4d7a1c53
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2023-07-04 15:47:48 +0700

    fixed unmounted error:

--- src/components/pages/Flashcards.tsx
+++ src/components/pages/Flashcards.tsx
@@ -1,34 +1,5 @@
-import { useState } from "react";
-import "./Flashcards.css";
-import { FlashcardList } from "../FlashcardList";
-
-export default function Flashcards() {
-  const [flashcards] = useState(SAMPLE_FLASHCARDS);
-  return (
-    <>
-      <div className="container">
-        <FlashcardList flashcards={flashcards} />
-      </div>
-    </>
-  );
+function Flashcards() {
+  return <div>Flashcards</div>;
 }
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2 + 2",
-    answer: "4",
-    options: ["2", "3", "4"],
-  },
-  {
-    id: 2,
-    question: "What is 2 + 3",
-    answer: "5",
-    options: ["3", "4", "5"],
-  },
-  {
-    id: 3,
-    question: "What is 2",
-    answer: "a number",
-    options: ["idk", "a string", "a number"],
-  },
-];
+
+export default Flashcards;
