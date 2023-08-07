c0f54d0dd99e1b7eb90699fee4eceda73fe83f4e
Jweng88 <108978684+Jweng88@users.noreply.github.com>
2023-08-07 16:32:44 +0800

    Updated firebase settings

--- src/components/Firebase/useFirebaseConfig.tsx
+++ src/components/Firebase/useFirebaseConfig.tsx
@@ -35,13 +35,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA7IHAbCmT4EOG7OoHhTdr0FQFDXpnOEvc",
-  authDomain: "fir-test2-c05e0.firebaseapp.com",
-  projectId: "fir-test2-c05e0",
-  storageBucket: "fir-test2-c05e0.appspot.com",
-  messagingSenderId: "973722957189",
-  appId: "1:973722957189:web:8b346112e47e888d71c0ec",
-  measurementId: "G-C7NZCEN08P",
+  apiKey: "AIzaSyAs-ojjrnduxuL64nx671jh96FMSV9gFcQ",
+  authDomain: "fir-test-3-28019.firebaseapp.com",
+  projectId: "fir-test-3-28019",
+  storageBucket: "fir-test-3-28019.appspot.com",
+  messagingSenderId: "745467662412",
+  appId: "1:745467662412:web:b5df661bd6358afce1e4e9",
+  measurementId: "G-N5NR30JGKD",
 };
 
 // Initialize Firebase
